11242518545a0d74a9fe76576fac94edac0d0cd2
linestandard <joelin0@yahoo.com>
2017-10-26 00:22:34 -0700

    Change from Windows.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sample-console-app
 Sample .NET Core console application
+
+Learn how to use GitHub with Visual Studio.
--- SampleConsoleApp/SampleConsoleApp/Program.cs
+++ SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace SampleConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Change made from Windows.");
         }
     }
 }
